5ddadcf2213e75025fec328363a6962493db638f
Roman Vanesyan <roman@vanesyan.com>
2023-01-10 01:45:46 +0200

    feat(wezterm): set custom color scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,34 @@
 local wezterm = require('wezterm')
 
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'GitHub Dark'
+  else
+    -- TODO: set light theme. rn, font looks too thin when using builtin 'Github Light'. 
+    return 'GitHub Dark'
+  end
+end
+
+-- Automatically reload the schema based on OS appearance settings.
+wezterm.on('window-config-reloaded', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
+
 return {
-    font = wezterm.font('SF Mono'),
-    font_size = 13,
-    line_height = 1.1,
-    front_end='WebGpu'
+  -- General
+  front_end = 'WebGpu',
+
+  -- Fonts
+  font = wezterm.font('SF Mono'),
+  font_size = 13,
+  line_height = 1.1,
+
+  -- Colors
+  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 }
