cad60557faef2d7fa3c19bec35fe9d2c3f8ab558
Roman Vanesyan <roman@vanesyan.com>
2023-01-12 02:43:39 +0200

    feat(nvim): add highlights & github theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,3 +12,26 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('plugins')
+
+-- Show line numbers.
+-- At current line number is absolute number, surronding lines are relative numbers to current line.
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Highlight cursor line.
+vim.opt.cursorline = true
+
+-- TODO(roman@vanesyan.com): set proper styles.
+vim.api.nvim_set_hl(0, 'CursorLine', { ctermbg = 242 })
+vim.api.nvim_set_hl(0, 'CursorLineNr', { bold = true })
+
+vim.opt.colorcolumn = '80,120'
+
+-- Highlight on copy (yank).
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end
+})
