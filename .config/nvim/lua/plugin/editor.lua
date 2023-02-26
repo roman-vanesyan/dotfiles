50533a3fcdff0fda962e038b14922e61ae64de94
Roman Vanesyan <roman@vanesyan.com>
2023-02-26 23:36:30 +0200

    feat(nvim): add setup keybindings suggestions & drop lock file

--- .config/nvim/lua/plugin/editor.lua
+++ .config/nvim/lua/plugin/editor.lua
@@ -25,4 +25,13 @@ return {
       },
     },
   },
+
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require('which-key').setup({})
+    end,
+  }
 }
