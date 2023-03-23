73e63d5fc07a88c4d8e73099faf21b6a063cd435
Roman Vanesyan <roman@vanesyan.com>
2023-03-23 02:11:45 +0200

    fix(nvim): fix scrolling hotkeys for cmp

--- .config/nvim/lua/plugin/completion.lua
+++ .config/nvim/lua/plugin/completion.lua
@@ -50,7 +50,7 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-i>"] = cmp.mapping.scroll_docs(4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
