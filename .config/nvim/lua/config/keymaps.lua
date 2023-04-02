86d8c5d95173049911dcddfa206de26a98bd616b
Roman Vanesyan <roman@vanesyan.com>
2023-04-02 22:27:52 +0300

    feat(nvim): add some keybindings for LSP

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -19,3 +19,24 @@ map({ "i" }, "{", "{}<left>")
 map({ "i" }, "<", "<><left>")
 map({ "i" }, "{<cr>", "{<cr>}<left><left><Esc>O")
 map({ "i" }, "{;<cr>", "{<cr>};<left><left><reft><Esc>O")
+
+-- LSP
+map(
+  { "n", "v" },
+  "gd",
+  "<cmd>Telescope lsp_definitions<cr>",
+  { desc = "Go to declaration" }
+)
+map(
+  { "n", "v" },
+  "gD",
+  "<cmd>vim.lsp.buf.declaration<cr>",
+  { desc = "Goto Declaration" }
+)
+
+map(
+  { "n", "v" },
+  "<leader>rn",
+  "<cmd>lua vim.lsp.buf.rename()<CR>",
+  { desc = "Rename definition" }
+)
